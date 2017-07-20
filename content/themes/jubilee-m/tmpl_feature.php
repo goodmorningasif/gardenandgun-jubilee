2a06f06499f4044ecf611c0b1adef16c5e079f95
Asif Ahmed <asif@stitchdesignco.com>
2017-07-20 18:17:04 -0400

    Add nav data to store.php

--- content/themes/jubilee-m/tmpl_feature.php
+++ content/themes/jubilee-m/tmpl_feature.php
@@ -9,11 +9,25 @@
 $compiler = include('compiler.php');
 $data = include('store.php');
 
+// if(have_rows('feat_flexible')) :
+// 	while(have_rows('feat_flexible')): 
+// 		the_row();
+
+// 	  $data['feature'] = array(
+// 	  	'permalink' => get_permalink(),
+// 	  	'title' => get_the_title(),
+// 	  	'content' => get_the_content()
+// 	  );
+
+// endwhile;endif;
+
+$data['feature'] = get_field('feat_flexible');
+
 echo $compiler->render('tmpl_feature', $data);
 
 echo '=========================';
 echo '<pre>';
-print_r($data['assets']['ft_menu']);
+print_r($data['assets']['nav']);
 echo '</pre>';
 
 ?>
