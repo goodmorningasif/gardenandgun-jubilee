acafd4ad40dfcef7304d2da3272a45d35336f287
Asif Ahmed <asif@stitchdesignco.com>
2017-07-22 21:37:44 -0400

    feed data for features pages

--- content/themes/jubilee-m/tmpl_feature.php
+++ content/themes/jubilee-m/tmpl_feature.php
@@ -9,25 +9,22 @@
 $compiler = include('compiler.php');
 $data = include('store.php');
 
-// if(have_rows('feat_flexible')) :
-// 	while(have_rows('feat_flexible')): 
-// 		the_row();
+$page_ID = get_the_ID();
 
-// 	  $data['feature'] = array(
-// 	  	'permalink' => get_permalink(),
-// 	  	'title' => get_the_title(),
-// 	  	'content' => get_the_content()
-// 	  );
+$data['page']['page_id'] = $page_ID;
 
-// endwhile;endif;
+$data['page']['gallery'] = array(
+	'toggle' => get_field('enable_gallery', $page_ID),
+	'images' => get_field('gallery', $page_ID)
+);
 
-$data['feature'] = get_field('feat_flexible');
+$data['page']['layouts'] = get_field('feat_flexible', $page_ID);
 
 echo $compiler->render('tmpl_feature', $data);
 
 echo '=========================';
 echo '<pre>';
-print_r($data['assets']);
+print_r($data['page']['layouts']);
 echo '</pre>';
 
 ?>
